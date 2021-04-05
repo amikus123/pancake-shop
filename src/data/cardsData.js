a4e05b8f0b22372bee7894b8d0a2a68f4ffdd86f
Amadeusz <amikusek123@gmail.com>
2021-04-05 20:10:14 +0200

    added multiple pictures

--- src/data/cardsData.js
+++ src/data/cardsData.js
@@ -1,16 +1,20 @@
+import p1 from "../images/data/cards/1.png";
+import p2 from "../images/data/cards/2.png";
+import p3 from "../images/data/cards/3.png";
+
 const cards = [
   {
-    img:"/products/1.jpg",
+    img:p1,
     header:"Unrivalled passion",
     text:"Proin lacus orci, sodales sit amet erat quis, placerat mattis mi. Aenean interdum tortor ut lorem hendrerit, in mollis mi rhoncus.",
   },
   {
-    img:"/products/1.jpg",
+    img:p2,
     header:"Amazing speed",
     text:" Nulla at enim a ipsum eleifend dignissim. Nunc sed augue facilisis, placerat turpis ut, euismod nisl.",
   },
   {
-    img:"/products/1.jpg",
+    img:p3,
     header:"Constant innovation",
     text:"Destibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;",
   },
--- src/data/pancakesData.js
+++ src/data/pancakesData.js
@@ -1,38 +1,53 @@
-const pancakes = [
-{
-name:"Pancake with strawberries",
-price:"22",
-img:"/products/1.jpg",
-rating:"4.5",
+import p1 from "../images/data/pancakes/1.png";
+import p2 from "../images/data/pancakes/2.png";
+import p3 from "../images/data/pancakes/3.png";
+import p4 from "../images/data/pancakes/4.png";
+import p5 from "../images/data/pancakes/5.png";
+import p6 from "../images/data/pancakes/6.png";
+import p7 from "../images/data/pancakes/7.png";
 
-},
-{
-  name:"Pancake with berries",
-  price:"28",
-  img:"/products/1.jpg",
-  rating:"2.2",
-  
+const pancakes = [
+  {
+    name: "Pancake with strawberries",
+    price: "22",
+    img: p1,
+    rating: "4.5",
   },
   {
-    name:"Pancake with chocolate",
-    price:"19",
-    img:"/products/1.jpg",
-    rating:"3.2",
-    
-    },
-    {
-      name:"Pancake with fish",
-      price:"22",
-      img:"/products/1.jpg",
-      rating:"5.0",
-      
-      },
-      {
-        name:"Pancake with bananas",
-        price:"16",
-        img:"/products/1.jpg",
-        rating:"4.6",
-        
-        },
-]
+    name: "Pancake with berries",
+    price: "28",
+    img: p2,
+    rating: "2.2",
+  },
+  {
+    name: "Pancake with chocolate",
+    price: "19",
+    img: p3,
+    rating: "3.2",
+  },
+  {
+    name: "Pancake with fish",
+    price: "22",
+    img: p4,
+    rating: "5.0",
+  },
+  {
+    name: "Pancake with bananas",
+    price: "16",
+    img: p5,
+    rating: "4.1",
+  },
+  {
+    name: "Pancake with strawberries",
+    price: "26",
+    img: p6,
+    rating: "4.8",
+  },
+  {
+    name: "Pancake with panckaes",
+    price: "17",
+    img: p7,
+    rating: "4.2",
+  },
+];
 export default pancakes;
