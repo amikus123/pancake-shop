d2722c453bc4f44decdc4bdd048dde9fb0057871
Amadeusz <amikusek123@gmail.com>
2021-04-11 18:33:01 +0200

    intersetcion ratio dispalys in wchich section you are

--- src/components/menu/products/PancakeCategory.js
+++ src/components/menu/products/PancakeCategory.js
@@ -1,32 +1,34 @@
 import React, { useEffect } from "react";
 import ProductFrame from "../../general/ProductFrame";
-import { useInView,InView  } from 'react-intersection-observer';
+import { useInView  } from 'react-intersection-observer';
 
 function PancakeCategory({ data,update,vis }) {
 
   const { list, category } = data;
-  const { ref, inView, entry } = useInView({
+  let { ref, inView, entry } = useInView({
     /* Optional options */
-    threshold: 0.5,
+    threshold: [0,0,1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],
+    rootMargin: "-100px 0px 0px 0px",
+    // delay:100,
   });
   useEffect(()=>{
     // console.log( inView, entry)
     if(entry){
       if(inView){
-        update(entry.intersectionRatio)
+        update(Number(entry.intersectionRatio.toPrecision(1)))
       }else{
         update(0)
-
       }
-
+      console.log(entry)
     }
   },[inView,update,entry])
+
   return (
        <section className="category" id={category} ref={ref}>
        <div className="category-heading" >
          <h2> {category}</h2>
          <h2>{`Header inside viewport ${inView}.`}</h2>
-         <h2>{vis}</h2>
+         <h2>{entry?entry.intersectionRatio:null}</h2>
 
        </div>
        <div className="category-content">
